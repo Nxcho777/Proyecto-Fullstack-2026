2a3ae84cc1879e38057b842e5702712d6440f319
FAGG1 <fe.guajardog@duocuc.cl>
2026-07-11 19:56:42 -0400

    refactor: eliminar microservicios fuera del alcance ET
    Se elimino en  microservicio adicionale de diacnostico y los archivos antiguos relacionados con su ejecución y documentación.

--- python-proyecto-2026/microservicio-diagnosticos/src/main/java/com/example/microservicio_diagnosticos/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.microservicio_diagnosticos.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
